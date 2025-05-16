60503d5f164edbc46463da669cfc565579b06b42
shin32125 <shintaga32123@gmail.com>
2025-05-16 22:03:37 +0900

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,8 @@
 #
-# Copyright (C) 2018-2021 PixysOS
+# Copyright (C) 2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_Spacewar.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_Spacewar-eng \
-    lineage_Spacewar-userdebug \
-    lineage_Spacewar-user
