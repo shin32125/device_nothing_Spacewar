04ad374f63431986228170cb003b063c1dbfc4e9
shin32125 <shintaga32123@gmail.com>
2025-05-20 04:36:10 +0900

    d

--- device.mk
+++ device.mk
@@ -229,7 +229,6 @@ PRODUCT_COPY_FILES += \
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.default \
-    fstab.zram \
     fstab.default.vendor_ramdisk \
     init.class_main.sh \
     init.kernel.post_boot.sh \
