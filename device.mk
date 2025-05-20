dfaabb9fc1b26b6f96c5e5c9d66f24aa587416d1
shin32125 <shintaga32123@gmail.com>
2025-05-20 21:36:01 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -15,6 +15,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/nothing/Spacewar/Spacewar-vendor.mk)
 
+# NT Camera
+$(call inherit-product, vendor/nothing/camera/nothing-camera.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
