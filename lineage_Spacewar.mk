5eeba8ed153341fbbff96b12f199d5391b8c02db
shin32125 <87742105+shin32125@users.noreply.github.com>
2025-07-29 11:14:01 +0900

    Revert "Update lineage_Spacewar.mk"
    This reverts commit b48b4443acf5968417c5b42016f7d93ed923c3ee.

--- lineage_Spacewar.mk
+++ lineage_Spacewar.mk
@@ -15,8 +15,6 @@ $(call inherit-product, device/nothing/Spacewar/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-WITH_GMS := true
-
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_Spacewar
 PRODUCT_DEVICE := Spacewar
