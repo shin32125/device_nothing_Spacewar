0da4776413a2ea1f4a2c8d13bad51eec39c680ea
shin32125 <87742105+shin32125@users.noreply.github.com>
2025-07-29 11:05:02 +0900

    Update lineage_Spacewar.mk

--- lineage_Spacewar.mk
+++ lineage_Spacewar.mk
@@ -16,6 +16,7 @@ $(call inherit-product, device/nothing/Spacewar/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 WITH_GMS := true
+TARGET_USES_PICO_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_Spacewar
